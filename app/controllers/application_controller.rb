7eb79bcbd67ca3fcc8e507c0538311cbeea4ba75
asukaaasaaa <azalea.seras@gmail.com>
2023-06-04 23:34:35 +0000

    [add]book2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
@@ -15,5 +15,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
 
 end
